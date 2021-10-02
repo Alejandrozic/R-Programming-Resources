d24cfe964a7130d5d0b8fd55935ee7c0320bb297
Alejandrozic <alejandrozic@gmail.com>
2021-10-02 13:29:16 -0400

    Update chapter-6.r

--- chapter-6.r
+++ chapter-6.r
@@ -42,17 +42,19 @@ qnorm(0.25, mean, sd, lower.tail = FALSE)
 # CONTINUOUS #
 # F(x) = x/8 where 0<=x<=4
 f <- function(x) {x/8}
+lower_limit <- 0
+upper_limit <- 4
 
 # P (x <= 1)
-integrate(f, lower = 0, upper = 1)$value
+integrate(f, lower = lower_limit, upper = 1)$value
 
 # P (x > 3)
-1 - integrate(f, lower = 0, upper = 3)$value
+1 - integrate(f, lower = lower_limit, upper = 3)$value
 # or
-integrate(f, lower = 3, upper = 4)$value
+integrate(f, lower = 3, upper = upper_limit)$value
 
 # P (x > 4)
-integrate(f, lower = 4, upper = 4)$value
+integrate(f, lower = 4, upper = upper_limit)$value
 
 # P (2 <= X <= 3)
 integrate(f, lower = 2, upper = 3)$value
@@ -60,13 +62,11 @@ integrate(f, lower = 2, upper = 3)$value
 # P (X <=2 | X<=3)
 a <- 2
 b <- 3
-(a^2 * integrate(f, lower = a-1, upper = 4)$value) / (b^2 * integrate(f, lower = a-1, upper = 4)$value)
+(a^2 * integrate(f, lower = a-1, upper = upper_limit)$value) / (b^2 * integrate(f, lower = a-1, upper = upper_limit)$value)
 
 # P (X <= c) = 0.5
 target_density <- 0.5
-lower_limit <- 0
-upper_limit <- 4
-find_upper_bnd <- function(f, interval, target) {
+find_upper_bnd<- function(f, interval, target) {
     optimize(function(x) {
         abs(integrate(f, lower_limit, x)$value-target)
     }, interval)$minimum
