14f2199e7fa1f6cab72a3fb40eddf3d79ab809ea
Alejandrozic <alejandrozic@gmail.com>
2021-10-04 20:32:58 -0400

    Create chapter-6-3.r

---
+++ chapter-6-3.r
@@ -0,0 +1,10 @@
+# --------- #
+# -- 6.3 -- #
+# Checking The Normality Assumption
+# --------- #
+
+# PREREQ IMPORT FILES
+path_tunnel <- "ex06-93tunnel.csv"
+path_oxyben <- "ex06-94oxyben.csv"
+read.table(path_tunnel, header=TRUE, sep=",", stringsAsFactors=FALSE)
+read.table(path_oxyben, header=TRUE, sep=",", stringsAsFactors=FALSE)
