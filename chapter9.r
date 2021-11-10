96e85ee6697df229fceaf5a118b0f2ae49147f8e
Alejandrozic <alejandrozic@gmail.com>
2021-11-10 15:53:29 -0500

    Update chapter9.r

--- chapter9.r
+++ chapter9.r
@@ -1,33 +1,65 @@
 ##################################################################
-# 9.4
+# 9
 #
 
-mean 		<- 44.4	# From trials
-n 		<- 14	# From trials
-x 		<- 40	# Provided as u
+# Z-Distribution
+
+mean 		<- 44.4	# From trials [Sample Mean]
+n 		<- 14		# From trials [Number of Trials]
+x 		<- 40		# Provided as u
 standard_d 	<- 3.384231	# Provided with u
 
-# Find z-stat
+# Find test statistic / Z Stat
 
-z_stat <- (mean - x) / ( standard_d / sqrt(n) )
+test_stat <- (mean - x) / ( standard_d / sqrt(n) )
 
 # Find p-value
 
 # -- if inequality Ha is "greater then"|">"
-p_value <- 1 - pnorm(z_stat, mean = 0, sd = 1)
+p_value <- 1 - pnorm(test_stat, mean = 0, sd = 1)
 
 # -- if inequality Ha is "no equal to"|"!=" and z_stat is positive
-p_value <- 2 * ( 1 - pnorm(z_stat, mean = 0, sd = 1) )
+p_value <- 2 * ( 1 - pnorm(test_stat, mean = 0, sd = 1) )
 
 # -- if inequality Ha is "no equal to"|"!=" and z_stat is negative
-p_value <- 2 *  pnorm(z_stat, mean = 0, sd = 1) 
+p_value <- 2 *  pnorm(test_stat, mean = 0, sd = 1) 
 
 # -- if inequality Ha is "less then"|"<"
-p_value <- pnorm(z_stat, mean = 0, sd = 1)
+p_value <- pnorm(test_stat, mean = 0, sd = 1)
 
-##################################################################
-# 9.5
-#
+#################################################
+
+mean 		<- 44.4	# From trials [Sample Mean]
+n 		<- 14		# From trials [Number of Trials]
+x 		<- 40		# Provided as u
+standard_d 	<- 3.384231	# Provided with u
+
+# T-Distribution
+
+# Find test statistic
+
+test_stat <- (mean - x) / ( standard_d / sqrt(n) )
+
+# Find T-Stat
+df <- n - 1
+
+# p_value_lwr <- pt(test_stat, df)  # Lower Tail Area
+# p_value_lwr <-1 - pt(test_stat, df)  # Upper Tail Area
+
+# -- if inequality Ha is "greater then"|">"
+p_value <- 1 - pt(test_stat, df)
+
+# -- if inequality Ha is "no equal to"|"!=" and z_stat is positive
+p_value <- 2 * ( 1 - pt(test_stat, df)) )
+
+# -- if inequality Ha is "no equal to"|"!=" and z_stat is negative
+p_value <- 2 *  pt(test_stat, df)
+
+# -- if inequality Ha is "less then"|"<"
+p_value <- pt(test_stat, df)
+
+
+#################################################
 
 # Find SE
 standard_d <- 0.7409
